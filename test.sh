bc609aadfd0eb1e65757e7550edf81560e8fe479
nir3shprabu <115976526+nir3shprabu@users.noreply.github.com>
2022-11-09 18:17:40 +0530

    Fix: release.yaml

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 
 set -o errexit -o nounset -o errtrace -o pipefail
 
-IMAGE_NAME=${IMAGE_NAME:-"ghcr.io/nir3shprabu"}
-CONTAINER_NAME=${CONTAINER_NAME:-"althttpd-smoketest-$(date +%s)"}
+IMAGE_NAME=${IMAGE_NAME:-"nir3shprabu/distroless-althttpd"}
+CONTAINER_NAME=${CONTAINER_NAME:-"distroless-althttpd-smoketest-$(date +%s)"}
 
 docker run -p 8080:80 -d --name $CONTAINER_NAME $IMAGE_NAME
 trap "docker rm -f $CONTAINER_NAME" EXIT
